f545e675767e229a382cbbdda69a816b844115fb
denis.ianhaki@escola.pr.gov.br <a2566109@campus.pg.utfpr.edu.br>
2023-06-05 20:40:01 -0300

    gdfg

--- trabalho3/denisborbasianhaki.c
+++ trabalho3/denisborbasianhaki.c
@@ -1,26 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h> // habilita acentuação
+#include <string.h>
+
+/*
+       ----------------
+       | Código:      |
+       ------------------------------------
+       | Nome:               | Ano:       |
+       ------------------------------------
+       | Cargo:             | ValorHora:  |
+       ------------------------------------
+*/
+struct Funcionarios
+{
+    int codigo;
+    char nome[50];
+    char cargo[50];
+    char ano[4];
+    double valorHora;
+    int DiaHoras[2][31];
+    // Matriz de dias com horas trabalhadas de cada dia
+};
 
 int main()
 {
-    /*
-        |   ----------------
-        |   | Código:      |
-        |   ------------------------------------
-        |   | Nome:               | Ano:       |
-        |   ------------------------------------
-        |   | Cargo:             | ValorHora:  |
-        |   ------------------------------------
-    */
-    struct funcionarios
-    {
-        int codigo;
-        char nome[50];
-        char cargo[50];
-        char ano[4];
-        double valorHora;
-    };
+
+    struct Funcionarios Funcionario1;
+
+    strcpy(Funcionario1.nome, "Nometeste");
+    strcpy(Funcionario1.cargo, "Pedreiro");
+    strcpy(Funcionario1.ano, "1940");
+    Funcionario1.valorHora, 15.5;
 
     return 0;
 }
