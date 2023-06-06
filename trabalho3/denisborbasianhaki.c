22e506badb76f4f1462875f6bfe758c0a83857b1
denis.ianhaki@escola.pr.gov.br <a2566109@campus.pg.utfpr.edu.br>
2023-06-05 21:14:11 -0300

    gdfg

--- trabalho3/denisborbasianhaki.c
+++ trabalho3/denisborbasianhaki.c
@@ -19,19 +19,15 @@ struct Funcionarios
     char cargo[50];
     char ano[4];
     double valorHora;
-    int DiaHoras[2][31];
-    // Matriz de dias com horas trabalhadas de cada dia
-};
+} Funcionario[50];
 
 int main()
 {
 
-    struct Funcionarios Funcionario1;
-
-    strcpy(Funcionario1.nome, "Nometeste");
-    strcpy(Funcionario1.cargo, "Pedreiro");
-    strcpy(Funcionario1.ano, "1940");
-    Funcionario1.valorHora, 15.5;
+    strcpy(Funcionario[1].nome, "Pedro");
+    strcpy(Funcionario[1].cargo, "Pedreiro");
+    strcpy(Funcionario[1].ano, "1940");
+    Funcionario[1].valorHora, 15.5;
 
     return 0;
 }
